597ad59c58385939413ae6260e6b4d2bd6836714
MrHKing <792679341@qq.com>
2018-04-03 00:08:26 +0800

    add mybaits

---
+++ zhiren-business/src/main/java/zhiren/mapper/DemoMapper.java
@@ -0,0 +1,17 @@
+package zhiren.mapper;
+
+import zhiren.model.Demo;
+
+public interface DemoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Demo record);
+
+    int insertSelective(Demo record);
+
+    Demo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Demo record);
+
+    int updateByPrimaryKey(Demo record);
+}
